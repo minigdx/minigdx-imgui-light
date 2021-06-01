5c1d3187a1f646119d05f759e026e4f9994190b6
David Wursteisen <david.wursteisen+git@gmail.com>
2021-06-01 09:53:46 +0200

    Add developer information

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,18 @@
 plugins {
     id("com.github.minigdx.gradle.plugin.developer.mpp") version "1.1.0-alpha0"
 }
+
+minigdxDeveloper {
+    this.name.set("imGUI Light")
+    this.description.set("Light implementation of a Immediate Mode GUI (imGUI)")
+    this.projectUrl.set("https://github.com/minigdx/minigdx-imgui-light")
+    this.licence {
+        name.set("MIT Licence")
+        url.set("https://github.com/minigdx/minigdx-imgui-light/blob/master/LICENSE")
+    }
+    developer {
+        name.set("David Wursteisen")
+        email.set("david.wursteisen+minigdx@gmail.com")
+        url.set("https://github.com/dwursteisen")
+    }
+}
